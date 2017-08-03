a495bf9f3046f909c134cbb051b5ba90d8f579ce
egmontas <egmontas@gmail.com>
2017-08-03 08:36:11 +0100

    shader script included...

---
+++ Scripts/CameraShaderToggle.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+
+public class CameraShaderToggle : MonoBehaviour
+{
+    public Shader ShaderOne;
+    
+    void OnEnable ()
+    {
+        if (ShaderOne != null)
+            GetComponent<Camera>().SetReplacementShader(ShaderOne, "RenderType");
+    } 
+
+    void OnDisable ()
+    {
+        GetComponent<Camera>().ResetReplacementShader();
+    }
+}
